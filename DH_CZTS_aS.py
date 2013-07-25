2c79bb88975ac5562cf6ab589eea23437a631c1f
Adam Jackson <a.j.jackson@bath.ac.uk>
2013-07-25 14:47:14 +0100

    Enthalpy calculator, setting up chemical potentials

--- DH_CZTS_aS.py
+++ DH_CZTS_aS.py
@@ -1,40 +1,53 @@
 """Calculate enthalpies of formation for CZTS over a range of temperatures at standard pressure."""
+import numpy as np
 from scipy import constants # Library provides 2010 CODATA values for physical constants
-from interpolate_thermal_property import get_potential_aims
+from interpolate_thermal_property import get_potential_aims, get_potential_nist_table
 from aims import pbesol_energy_eV, fu_cell  # Data file of FHI-aims computed systems and results
 
 def main():
+########## Alias physical constants ###########
+    eV2J = constants.physical_constants['electron volt-joule relationship'][0]
+    k = constants.physical_constants['Boltzmann constant in eV/K'][0]
+
 ################ Set conditions ###############
     T = 298.15 # K
     P = 1e5    # Pa
 ############# Data and parameters from DFT calcs ################
  
     ### Copy energies and convert to J: ###
-    eV2J = constants.physical_constants['electron volt-joule relationship'][0]
     pbesol_energy_J = pbesol_energy_eV.copy()
     for species in pbesol_energy_eV:
         pbesol_energy_J[species] = pbesol_energy_eV[species]*eV2J*constants.N_A
     ### thermodynamic data for periodic solids ###
     thermo_file=dict()
     U_func = dict()
+    H_func = dict()
+    TS_func = dict()
+    S_func = dict()
 
     phonon_species = ('czts','Cu','Zn','Sn','alpha_S')
     for species in phonon_species:
         thermo_file[species] = 'phonopy_output/' + species + '.dat'
         U_func[species] = get_potential_aims(thermo_file[species],'U')
+        TS_func[species] = get_potential_aims(thermo_file[species],'TS')
 
     ### thermodynamic data for non-periodic components ###
-    gas_species = ('S8')
+    gas_species = ['S8']
     for species in gas_species:
-
-        print 0
-
+        thermo_file[species] = 'nist_janaf/' + species + '.dat'
+        H_func[species] = get_potential_nist_table(thermo_file[species],'H')
+        S_func[species] = get_potential_nist_table(thermo_file[species],'S')
+        
 ############## Enthalpy for each species #####################
     V = 1E-30 # Need to import correct cell volume from geometry file
     H = dict()
+    mu = dict()
     for species in phonon_species:
-        H[species] = pbesol_energy_eV[species] + U_func[species](T)*eV2J*constants.N_A + P*V
-
+        H[species] = pbesol_energy_eV[species] + U_func[species](T) + (P*V)/(eV2J*constants.N_A)
+        mu[species] = H[species] - TS_func[species](T)
+    for species in gas_species:
+        H[species] = pbesol_energy_eV[species] + (H_func[species](T))/(eV2J*constants.N_A) # Assuming ideality
+        mu[species] = H[species] - T*S_func[species](T) + k*T*np.log(P/1E5)
 ######### Calculate some formation energies! #################
     formation_alpha_eV = (pbesol_energy_eV['czts']/fu_cell['czts'] -
                           (2*pbesol_energy_eV['Cu']/fu_cell['Cu'] +
@@ -43,8 +56,21 @@ def main():
                            4*pbesol_energy_eV['alpha_S']/fu_cell['alpha_S']
                            ))
     print "Formation energy 2 Cu + Zn + Sn + 4 S(alpha) -> CZTS: {0:4.2f} eV".format(formation_alpha_eV)
+    
+    DH_S8_eV = (H['czts']/fu_cell['czts'] -
+                (2*H['Cu']/fu_cell['Cu'] +
+                 H['Zn']/fu_cell['Zn'] +
+                 H['Sn']/fu_cell['Sn'] +
+                 4*H['alpha_S']/fu_cell['alpha_S']
+                 ))
+
+    print "Formation enthalpy @ {0:3.5g} K, {1:3.3g} bar:".format(T,P/1E5)
+    print "2 Cu + Zn + Sn + 4 S(alpha) -> CZTS: {0:4.2f} eV".format(DH_S8_eV)
                     
     return 0
+
+
+
 
 if __name__=="__main__":
     main()
