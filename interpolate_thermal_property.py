9a72d4aaf4d4bcba64b3fe10cd23d7f3bb71af91
Adam Jackson <a.j.jackson@bath.ac.uk>
2013-07-25 17:06:10 +0100

    Standardised to J, mol, K. Added ideal gas correction for internal energy U

--- interpolate_thermal_property.py
+++ interpolate_thermal_property.py
@@ -23,20 +23,21 @@ def get_potential_aims(file,property):
     return thefunction
 
 def get_potential_nist_table(file, property):
-    """Thermodynamic property interpolation function. Requires NIST-JANAF table."""
+    """Thermodynamic property interpolation function. Requires NIST-JANAF table. All properties in J, mol and K"""
     data = genfromtxt(file,skip_header=2)
     T = data[:,0]
     if property in ('Cp','C','heat_capacity'):
         potential = data[:,1]
     elif property in ('S','entropy'):
         potential = data[:,2]
-    #
-    # Yes, this one looks strange. Both are normalised to U(0) = H(0) = 0 for easy addition to electronic energy.
-    # For enthalpy change from standard conditions, use 'DH'
-    elif property in ('H','enthalpy','U','internal_energy'): 
-        potential = data[:,4] - data[0,4]
+    elif property in ('H','enthalpy'): 
+        potential = (data[:,4] - data[0,4])*1E3
+    elif property in ('U','internal_energy'):  
+        # U = H - PV; for ideal gas molar PV = RT so U = H - RT
+        from scipy.constants import R as R
+        potential = (data[:,4] - data[0,4])*1E3 - R*data[:,0]
     elif property in ('DH','Delta_H','standard_enthalpy_change'):
-        potential = data[:,4]
+        potential = data[:,4]*1E3
     else:
         raise RuntimeError('Property not found')        
 
