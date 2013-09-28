76929a1651952788fb9f5db8356cd8fd6dbe1fdb
ajjackson <a.j.jackson@bath.ac.uk>
2013-09-28 15:09:51 +0100

    surface models with matplotlib. Charter tty font required.

---
+++ DG_CZTS_S2.py
@@ -0,0 +1,22 @@
+def main():
+    from materials import CZTS, Cu, Zn, Sn, S2
+    import numpy as np
+
+    from DG_CZTS_S8 import plot_potential
+
+    T = np.linspace(100,1000,100)    # K
+    P = np.array( np.logspace(3,7,100),ndmin=2).transpose() # Pa
+    
+    D_mu = CZTS.mu_kJ(T,P) - (2*Cu.mu_kJ(T,P) +
+                                    Zn.mu_kJ(T,P) +
+                                    Sn.mu_kJ(T,P) +
+                                    2*S2.mu_kJ(T,P)
+        )
+    
+    D_mu_label = '$\Delta G_f$ / kJ mol$^{-1}$'
+    scale_range = [-620,-240]
+    
+    plot_potential(T,P,D_mu,D_mu_label,scale_range)
+
+if __name__ == "__main__":
+    main()
--- DG_CZTS_S8.py
+++ DG_CZTS_S8.py
@@ -1,27 +1,40 @@
-from materials import CZTS, Cu, Zn, Sn, S8
-import matplotlib.pyplot as plt
-import numpy as np
+def main():
+    from materials import CZTS, Cu, Zn, Sn, S8
+    import numpy as np
+    T = np.linspace(100,1000,100)    # K
+    P = np.array( np.logspace(3,7,100),ndmin=2).transpose() # Pa
+    
+    D_mu = CZTS.mu_kJ(T,P) - (2*Cu.mu_kJ(T,P) +
+                                    Zn.mu_kJ(T,P) +
+                                    Sn.mu_kJ(T,P) +
+                                    0.5*S8.mu_kJ(T,P)
+        )
+    
+    D_mu_label = '$\Delta G_f$ / kJ mol$^{-1}$'
+    scale_range = [-380,-240]
+    
+    plot_potential(T,P,D_mu,D_mu_label,scale_range)
 
-T = np.linspace(100,1000,10)    # K
-P = np.array( np.logspace(3,7,10),ndmin=2).transpose() # Pa
-
-D_mu = CZTS.mu_kJ(T,P) - (2*Cu.mu_kJ(T,P) +
-                          Zn.mu_kJ(T,P) +
-                          Sn.mu_kJ(T,P) +
-                          0.5*S8.mu_kJ(T,P)
-                          )
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-a = plt.contour(T,P.flatten(),D_mu,10, linewidths = 0.5, colors = 'k')
-plt.pcolormesh(T,P.flatten(),D_mu,cmap=plt.get_cmap('BuPu'),vmin=-400, vmax=-220)
-colours = plt.colorbar()
-plt.xlabel('Temperature / K')
-plt.ylabel('Pressure / Pa')
-colours.set_label('$\Delta\mu_f / $kJ mol$^{-1}$')
-ax.set_yscale('log')
-plt.clabel(a)
-plt.show()
-
-
+def plot_potential(T,P,potential,potential_label,scale_range):
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    mpl.rcParams['font.family'] = 'serif'
+    mpl.rcParams['font.serif'] = ['Charter']
+    mpl.rcParams['font.size'] = 16
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    a = plt.contour(T,P.flatten(),potential,10, linewidths = 1, colors = 'k')
+    plt.pcolormesh(T,P.flatten(),potential,
+                   cmap=plt.get_cmap('BuPu'),vmin=scale_range[0], vmax=scale_range[1])
+    colours = plt.colorbar()
+    plt.xlabel('Temperature / K')
+    plt.ylabel('Pressure / Pa')
+    colours.set_label(potential_label)
+    ax.set_yscale('log')
+    plt.clabel(a)
+    
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
