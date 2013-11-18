743a7879673de391fb75d294fb72c1760ff440a1
ajjackson <a.j.jackson@bath.ac.uk>
2013-11-18 01:53:05 +0000

    new plots incl. binary.

--- DG_CZTS_SnS2.py
+++ DG_CZTS_SnS2.py
@@ -1,5 +1,5 @@
 def main():
-    from materials import CZTS, Cu, Zn, Sn, S2
+    from materials import CZTS, Cu2S_low, ZnS_zincblende, Sn, S2
     import numpy as np
 
     from DG_CZTS_S8 import plot_potential
@@ -7,14 +7,13 @@ def main():
     T = np.linspace(100,1600,100)    # K
     P = np.array( np.logspace(3,7,100),ndmin=2).transpose() # Pa
     
-    D_mu = CZTS.mu_kJ(T,P) - (2*Cu.mu_kJ(T,P) +
-                                    Zn.mu_kJ(T,P) +
-                                    Sn.mu_kJ(T,P) +
-                                    2*S2.mu_kJ(T,P)
-        )
+    D_mu = CZTS.mu_kJ(T,P) - (Cu2S_low.mu_kJ(T,P) +
+                              ZnS_zincblende.mu_kJ(T,P) +
+                              Sn.mu_kJ(T,P) +
+                              S2.mu_kJ(T,P) )
     
     D_mu_label = '$\Delta G_f$ / kJ mol$^{-1}$'
-    scale_range = [-650,50]
+    scale_range = [-300,100]
     
     plot_potential(T,P,D_mu,D_mu_label,scale_range)
 
---
+++ DG_binaries_CZTS_S2.py
@@ -0,0 +1,21 @@
+def main():
+    from materials import CZTS, Cu2S_low, ZnS_zincblende, SnS2
+    import numpy as np
+
+    from DG_CZTS_S8 import plot_potential
+
+    T = np.linspace(100,1000,100)    # K
+    P = np.array( np.logspace(3,7,100),ndmin=2).transpose() # Pa
+    
+    D_mu = CZTS.mu_kJ(T,P) - (Cu2S_low.mu_kJ(T,P) +
+                              ZnS_zincblende.mu_kJ(T,P) +
+                              SnS2.mu_kJ(T,P)
+                                      )
+    
+    D_mu_label = '$\Delta G_f$ / kJ mol$^{-1}$'
+    scale_range = [-50,-40]
+    
+    plot_potential(T,P,D_mu,D_mu_label,scale_range)
+
+if __name__ == "__main__":
+    main()
