36883b8980688a9b6a6bf5fb80267ee33a0da7ff
ajjackson <a.j.jackson@bath.ac.uk>
2014-02-09 18:10:02 +0000

    Updated Cv sanity check plotter to include Stannite

--- cv_compare.py
+++ cv_compare.py
@@ -1,19 +1,18 @@
 import numpy as np
-from materials import Cu, Zn, Sn, alpha_S, CZTS
+from materials import Cu, Zn, Sn, alpha_S, CZTS_kesterite, CZTS_stannite
 from matplotlib import pyplot as plt
-from matplotlib2tikz import save as tikz_save
 
 T = np.linspace(0,1200,100)
 
 for material in Cu, Zn, Sn, alpha_S:
     plt.plot(T,material.Cv_kB(T), label=material.name)
 
-plt.plot(T,CZTS.Cv_kB(T)/8, label=CZTS.name)
+for material in CZTS_kesterite, CZTS_stannite:
+    plt.plot(T,material.Cv_kB(T)/8, label=material.name)
 
 plt.axis([0,1200,0,3])
 plt.xlabel('Temperature (K)')
 plt.ylabel('$C_v$ ($k_B$ atom$^{-1}$)')
 plt.legend()
-plt.savefig('Cv.pdf')
-tikz_save("Cv.tex",figureheight="8cm",figurewidth="10cm")
-#plt.show()
+plt.savefig('plots/Cv.png')
+plt.show()
