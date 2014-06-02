cfd87ce327f4d5b27257fd97330c1eb1fee4c605
Adam Jackson <a.j.jackson@bath.ac.uk>
2014-06-02 10:38:16 +0100

    Ternary results added to sanity checks, linecycler implemented for clarity

--- sanity_checks/cv_compare.py
+++ sanity_checks/cv_compare.py
@@ -1,17 +1,23 @@
 import numpy as np
 from materials import Cu, Zn, Sn, alpha_S, CZTS_kesterite, CZTS_stannite
 from materials import Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma
+from materials import Cu2SnS3_mo1, Cu2SnS3_mo2
 from matplotlib import pyplot as plt
+
+from itertools import cycle
+lines = ["-","--","-."]
+linecycler = cycle(lines)
 
 T = np.linspace(0,1200,100)
 
 for material in (Cu, Zn, Sn, alpha_S, CZTS_kesterite, CZTS_stannite,
-                 Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma):
-    plt.plot(T,material.Cv_kB(T)/material.N, label=material.name)
+                 Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma,
+                 Cu2SnS3_mo1, Cu2SnS3_mo2):
+    plt.plot(T,material.Cv_kB(T)/material.N, next(linecycler), label=material.name)
 
 plt.axis([0,1200,0,3])
 plt.xlabel('Temperature (K)')
 plt.ylabel('$C_v$ ($k_B$ atom$^{-1}$)')
 plt.legend()
-plt.savefig('plots/Cv.png')
+plt.savefig('plots/Cv.pdf')
 plt.show()
--- sanity_checks/mu_compare.py
+++ sanity_checks/mu_compare.py
@@ -1,19 +1,25 @@
 import numpy as np
 from materials import Cu, Zn, Sn, alpha_S, CZTS_kesterite, CZTS_stannite
 from materials import Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma
+from materials import Cu2SnS3_mo1, Cu2SnS3_mo2
 from matplotlib import pyplot as plt
+
+from itertools import cycle
+lines = ["-","--","-."]
+linecycler = cycle(lines)
 
 T = np.linspace(0,1200,100)  # K
 P = 1E5                      # Pa
 
 for material in (Cu, Zn, Sn, alpha_S, CZTS_kesterite, CZTS_stannite,
-                 Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma):
-    plt.plot(T,(material.mu_kJ(T,P)-material.mu_kJ(0,P))/material.N,
+                 Cu2S_low, ZnS_zincblende, ZnS_wurtzite, SnS2, SnS_pcma,
+                 Cu2SnS3_mo1, Cu2SnS3_mo2):
+    plt.plot(T,(material.mu_kJ(T,P)-material.mu_kJ(0,P))/material.N,next(linecycler),
              label=material.name)
 
 #plt.axis([0,1200,0,3])
 plt.xlabel('Temperature (K)')
 plt.ylabel('$\Delta \mu$ (kJ atom mol$^{-1}$)')
-plt.legend()
-plt.savefig('plots/Delta_mu.png')
+plt.legend(loc=3)
+plt.savefig('plots/Delta_mu.pdf')
 plt.show()
