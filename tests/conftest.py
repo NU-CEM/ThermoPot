bcfc2a4f7c4a62643c44179408b29a42cad3aa6d
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-09-13 15:31:39 +0100

    fix spacing

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,11 @@ def BaS_hybrid_calculation():
         os.path.join(os.path.dirname(__file__), "data/BaS_hse06_outfile")
     )
 
-
 @pytest.fixture()
 def BaS_pbesol_calculation():
     return calculations.AimsCalculation(
         os.path.join(os.path.dirname(__file__), "data/BaS_pbesol_relax_outfile")
     )
-
 
 @pytest.fixture()
 def BaS_solid():
@@ -25,7 +23,6 @@ def BaS_solid():
         os.path.join(os.path.dirname(__file__), "data/BaS_phonons"),
         BaS_pbesol_calculation,
     )
-
 
 @pytest.fixture()
 def BaS_BaS_reaction():
