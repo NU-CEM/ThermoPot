9c6b3a659a4a71cc89d8334b2612d61bca20a920
Lucy Whalley <l.whalley@northumbria.ac.uk>
2023-11-30 09:40:52 +0000

    shift the S2/S8 boundary when celsius is selected as units

--- thermopot/potential.py
+++ thermopot/potential.py
@@ -94,7 +94,7 @@ class Potential:
             x = T_tr(pressure).flatten()
 
             if T_units == "C":
-                x = self.x - 273.15
+                x = x - 273.15
 
             plt.plot(x, y_values, "k--", linewidth=3)
             plt.xlim(min(x_values), max(x_values))
@@ -110,7 +110,7 @@ class Potential:
                     linewidth=0,
                     edgecolor="0.8",
                 )
-                x1 = [1, 419.1596]
+                x1 = [1, 419.1596- 273.15]  # LW: I have shifted this without fully understanding the logic of x1,y1,y2...
                 y1 = [10000000, 10000000]
                 y2 = [0.001, 0.001]
                 plt.fill_between(
